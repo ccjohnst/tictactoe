765cc2aef186302e54eeb820c770f92b97d3b29d
Chris Johnston <christopherjohnston@protonmail.com>
2021-03-27 16:43:46 +0000

    begun creating display logic and added HTML grid for each item to be displayed on.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
-// You’re going to store the gameboard as an array inside of a Gameboard object, so start there! 
-//  Rule of thumb: if you only ever need ONE of something (gameBoard, displayController),
-//  use a module. If you need multiples of something (players!), create them with factories.
+/* You’re going to store the gameboard as an array inside of a 
+Gameboard object, so start there! 
+Rule of thumb: if you only ever need ONE of 
+something (gameBoard, displayController),
+use a module. If you need multiples of something (players!), 
+create them with factories.
+*/
 
-function createPlayer() {
+const createPlayers = (name) => {
     return {
-
+        name,
     }
 }
+
+
+/* create a module for our gameBoard
+TO DO: 
+- Store gameboard as array inside of Gameboard object*/
+
+const gameBoard = (() => {
+    const gameBoardArray = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+
+    const addToBox = () => {
+        gameBoardArray.forEach((item, index) => {
+            const box = document.querySelector(`#box${index}`);
+            box.textContent = item;
+        })
+    }
+    return addToBox();
+})();
+
+// const displayController = (() => {
+
+//     // append values of variables to boxes in html
+//     const addToBox = () => {
+//         gameBoard.gameBoardArray.forEach((item) => {
+//             console.log(item);
+//         })
+
+//     };
+//     return {
+
+//     };
+// })();
