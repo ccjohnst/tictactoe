7947769c733e02f55ee6c05950fec5a5fe248bed
Chris Johnston <christopherjohnston@protonmail.com>
2021-03-27 11:58:49 +0000

    Added begining of code to JS script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// You’re going to store the gameboard as an array inside of a Gameboard object, so start there! 
+//  Rule of thumb: if you only ever need ONE of something (gameBoard, displayController),
+//  use a module. If you need multiples of something (players!), create them with factories.
+
+function createPlayer() {
+    return {
+
+    }
+}
